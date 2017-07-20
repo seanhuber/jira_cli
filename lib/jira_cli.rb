94e13802fb86dac14da801f7f6e7c0ef7d11bf3b
Sean Huber <seanhuber@seanhuber.com>
2017-07-20 11:00:15 -0500

    initial commit

---
+++ lib/jira_cli.rb
@@ -0,0 +1,5 @@
+require "jira_cli/version"
+
+module JiraCli
+  # Your code goes here...
+end
---
+++ spec/jira_cli_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe JiraCli do
+  it "has a version number" do
+    expect(JiraCli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
