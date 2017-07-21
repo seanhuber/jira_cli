d3a7e9606f93a886d616746788c72705db53b501
Sean Huber <seanhuber@seanhuber.com>
2017-07-21 10:58:16 -0500

    spec: get_workflow_list

--- spec/jira_cli/wrapper_spec.rb
+++ spec/jira_cli/wrapper_spec.rb
@@ -9,4 +9,53 @@ RSpec.describe JiraCli::Wrapper do
     server_info = jira.get_server_info
     expect(server_info).to eq(stdout)
   end
+
+  it 'gets workflow list' do
+    stdout = '5 workflows in list
+"Name","Description","Last Modified","Last Modifier","Step Count","Default"
+"jira","The default JIRA workflow.","","","5","Yes"
+"PROJ1: Software Development Workflow","","02/27/2015 11:16 AM","Jane Doe","4","No"
+"PROJ2: Software Development Workflow","","02/27/2015 11:26 AM","Jane Doe","4","No"
+"PROJ3: Software Development Workflow","","02/27/2015 11:19 AM","Jane Doe","4","No"
+"PROJ4: Software Development Workflow","","02/27/2015 11:37 AM","Jane Doe","4","No"'
+    stderr = ''
+    status = ''
+    expect(Open3).to receive(:capture3).with("jira --action \"getWorkflowList\"").and_return([stdout, stderr, status])
+
+    expected_result = {
+                  "jira" => {
+            :description => "The default JIRA workflow.",
+             :step_count => 5,
+                :default => "Yes"
+      },
+      "PROJ1: Software Development Workflow" => {
+          :last_modified => "02/27/2015 11:16 AM",
+          :last_modifier => "Jane Doe",
+             :step_count => 4,
+                :default => "No"
+      },
+      "PROJ2: Software Development Workflow" => {
+          :last_modified => "02/27/2015 11:26 AM",
+          :last_modifier => "Jane Doe",
+             :step_count => 4,
+                :default => "No"
+      },
+      "PROJ3: Software Development Workflow" => {
+          :last_modified => "02/27/2015 11:19 AM",
+          :last_modifier => "Jane Doe",
+             :step_count => 4,
+                :default => "No"
+      },
+      "PROJ4: Software Development Workflow" => {
+          :last_modified => "02/27/2015 11:37 AM",
+          :last_modifier => "Jane Doe",
+             :step_count => 4,
+                :default => "No"
+      }
+    }
+
+    jira = JiraCli::Wrapper.new
+    workflow_list = jira.get_workflow_list
+    expect(workflow_list).to eq(expected_result)
+  end
 end
