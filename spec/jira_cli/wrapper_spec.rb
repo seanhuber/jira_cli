86e64c2aaa5dbe9d030fd884abc88041307ce4cb
Sean Huber <seanhuber@seanhuber.com>
2017-08-17 16:42:37 -0500

    spec: delete_issue_type_scheme

--- spec/jira_cli/wrapper_spec.rb
+++ spec/jira_cli/wrapper_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe JiraCli::Wrapper do
     end
   end
 
+  it 'deletes and issue type scheme' do
+    jira = JiraCli::Wrapper.new
+    output = "Issue type scheme with id 123 deleted."
+    expect_cli_request("jira --action \"deleteIssueTypeScheme\" --id \"123\"", output, output) do
+      jira.delete_issue_type_scheme id: 123
+    end
+  end
+
   it 'gets server info' do
     jira = JiraCli::Wrapper.new
     output = "JIRA version: 7.3.0, build: 73011, time: 1/3/17 12:00 AM, time zone: Central Standard Time, description: My Description, url: http://jira.<my_domain>.com"
