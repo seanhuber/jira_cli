4987da3ced282314c746c9480314213033574b3e
Sean Huber <seanhuber@seanhuber.com>
2017-08-17 16:54:12 -0500

    spec: delete_workflow

--- spec/jira_cli/wrapper_spec.rb
+++ spec/jira_cli/wrapper_spec.rb
@@ -47,6 +47,14 @@ RSpec.describe JiraCli::Wrapper do
     end
   end
 
+  it 'deletes a workflow' do
+    jira = JiraCli::Wrapper.new
+    output = "Workflow 'Software Simplified Workflow for Project MYPROJ' deleted."
+    expect_cli_request("jira --action \"deleteWorkflow\" --workflow \"Software Simplified Workflow for Project MYPROJ\"", output, output) do
+      jira.delete_workflow name: 'Software Simplified Workflow for Project MYPROJ'
+    end
+  end
+
   it 'gets server info' do
     jira = JiraCli::Wrapper.new
     output = "JIRA version: 7.3.0, build: 73011, time: 1/3/17 12:00 AM, time zone: Central Standard Time, description: My Description, url: http://jira.<my_domain>.com"
