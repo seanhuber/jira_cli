5a36259069d2d6c093e7726c5895c5b3b4066d16
Sean Huber <seanhuber@seanhuber.com>
2017-09-22 09:13:27 -0500

    Coveralls.wear! needs to come before everything else for full coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
+require 'coveralls'
+Coveralls.wear!
+
 require 'bundler/setup'
 require 'jira_cli'
 require 'ap'
 require 'yaml'
-require 'coveralls'
-
-Coveralls.wear!
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
